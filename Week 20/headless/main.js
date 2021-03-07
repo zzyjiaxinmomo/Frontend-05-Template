a4745495a6472276b32be1f0dfb8a195023c2c62
zzyjiaxinmomo <zzyjiaxinmomo@163.com>
2021-03-07 21:24:22 +0800

    final week

---
+++ Week 20/headless/main.js
@@ -0,0 +1,12 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080/main.html");
+  const a = await page.$("a");
+  console.log(await a.asElement().boxModel());
+  const imgs = await page.$$("img");
+  console.log(imgs);
+  debugger;
+})();
